b0154b015af690d09b819ed97fd6617d8373a2d7
janrendel <janrendel11@gmail.com>
2025-03-24 16:26:12 +0900

    4장 기본 위젯의 속성들

---
+++ app/src/main/java/kr/ac/kopo/widgettest/MainActivity.java
@@ -0,0 +1,42 @@
+package kr.ac.kopo.widgettest;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enable_clivkable_test);
+
+//        rg = findViewById(R.id.rg);
+//
+//        Button btnDone = findViewById(R.id.btn_done);
+//
+//        btnDone.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String result = "";
+//                if(rg.getCheckedRadioButtonId() == R.id.rb_male)
+//                    result = "남성";
+//                else
+//                    result = "여성";
+//
+//
+//                Toast toast = Toast.makeText(getApplicationContext(), result+"을 선택하였습니다.",Toast.LENGTH_LONG);
+//                toast.show();
+//            }
+//        });
+    }
+}
